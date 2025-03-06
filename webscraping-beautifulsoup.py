259ac053bf8ba3480b364c036835e223ac7d8905
Piyush2ShortHillsAI <piyush2@shorthills.ai>
2025-03-06 17:45:41 +0530

    first commit

--- webscraping-beautifulsoup.py
+++ webscraping-beautifulsoup.py
@@ -21,3 +21,28 @@ if response.status_code == 200:
         print(f"{idx}. {headline.text.strip()}")
 else:
     print("Failed to retrieve the webpage.")
+
+
+    """
+output
+Latest News Headlines:
+1. BEST OF PREMIUM
+2. Categories for you
+3. Top News
+4. Latest News
+5. Categories for you
+6. ICC Champions Trophy
+7. City News​
+8. Business
+9. ONLY IN THE EXPRESS
+10. SPORTS
+11. Latest Video
+12. Visual Stories
+13. AUDIO
+14. Technology
+15. Trending
+16. Education
+17. OPINION
+18. Today's Crossword
+
+    """ 
